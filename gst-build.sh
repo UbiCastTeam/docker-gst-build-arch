1caf57166b63568086469fc7be88bfecec72d6f0
Florent Thiéry <florent.thiery@ubicast.eu>
2018-07-19 14:00:43 +0200

    update meson opts syntax

--- gst-build.sh
+++ gst-build.sh
@@ -3,7 +3,7 @@ FOLDER=/opt/gstreamer
 mkdir -p $FOLDER
 git clone https://github.com/GStreamer/gst-build.git $FOLDER
 cd $FOLDER
-meson -Dpython=true -Dsharp=false -Drtsp_server=false -Dges=false -Dlibav=false build
+meson -Dpython=enabled -Dsharp=disabled -Drtsp_server=disabled -Dges=disabled -Dlibav=disabled build
 cd build
 ninja
 ln -sf /opt/gstreamer/gst-uninstalled.py /usr/local/bin
